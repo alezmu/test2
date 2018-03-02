17907002d715ca486571bf4c7481c283ee9cd91d
alezmu <aleksandra.m.zmudzinska@gmail.com>
2018-03-02 13:45:54 +0100

    Initial commit

---
+++ src/com/aleksandra/Main.java
@@ -0,0 +1,11 @@
+package com.aleksandra;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+        String tekst = "To jest drugi projekt ktry ma pokazac jak uzywac githuba";
+    }
+}
