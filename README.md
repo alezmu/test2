8e02957c68434c5893a7bd3e90f57edcd5093939
alezmu <aleksandra.m.zmudzinska@gmail.com>
2018-03-02 13:51:40 +0100

    some changes
    changes in project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test2
+Git Gui Client
--- src/com/aleksandra/Main.java
+++ src/com/aleksandra/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         String tekst = "To jest drugi projekt ktry ma pokazac jak uzywac githuba";
+        System.out.println("tekst");
     }
 }
